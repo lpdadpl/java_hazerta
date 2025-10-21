9336f26c427420757238a56667e45125979e8934
Alberto <albertodepablolopez@gmail.com>
2025-10-21 14:35:02 +0200

    Continuacion Ejercicios

---
+++ Ejercicios/src/ejerciciosString/ejercicio2/CadenaDeTextoUpper.java
@@ -0,0 +1,8 @@
+package ejerciciosString.ejercicio2;
+
+public class CadenaDeTextoUpper {
+    public static void main(String[] args) {
+        String cadenaDeTexto = "Nunca mates una mosca sobre la cabeza de un tigre.";
+        System.out.println(cadenaDeTexto.toUpperCase());
+    }
+}
