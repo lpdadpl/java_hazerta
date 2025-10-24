47def8388806822c54fa9ed3cbe6ee36d4d8e339
Alberto <albertodepablolopez@gmail.com>
2025-10-24 14:33:45 +0200

    Ejercicio1 ClaseAbstracta

---
+++ Ejercicios/src/ejerciciosHerenciaClaseAbstracta/ejercicio1/ProductoPerecedero.java
@@ -0,0 +1,63 @@
+package ejerciciosHerenciaClaseAbstracta.ejercicio1;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ProductoPerecedero extends Producto {
+    private LocalDate fCaducidad;
+    private LocalDate fFabricacion;
+
+    public ProductoPerecedero() {
+        super();
+    }
+
+    public ProductoPerecedero(String nombre, int cantidad, double pvp, LocalDate fFabricacion, LocalDate fCaducidad) {
+        super(nombre, cantidad, pvp);
+        this.fFabricacion = fFabricacion;
+        this.fCaducidad = fCaducidad;
+    }
+
+    public LocalDate getFCaducidad() {
+        return fCaducidad;
+    }
+
+    public void setFCaducidad(LocalDate fCaducidad) {
+        this.fCaducidad = fCaducidad;
+    }
+
+    public LocalDate getFFabricacion() {
+        return fFabricacion;
+    }
+
+    public void setFFabricacion(LocalDate fFabricacion) {
+        this.fFabricacion = fFabricacion;
+    }
+
+    public int tiempoConsumicion() {
+        if (fCaducidad != null && LocalDate.now().isBefore(fCaducidad)) {
+            return (int) ChronoUnit.DAYS.between(LocalDate.now(), fCaducidad);
+        } else {
+            System.out.println("el producto ha caducado");
+            return 0;
+        }
+    }
+    public int tiempoFabricacion(){
+        if (fFabricacion != null & LocalDate.now().isAfter((fFabricacion))) {
+            return (int) ChronoUnit.DAYS.between(LocalDate.now(), fFabricacion);
+        }else{
+            System.out.println("El producto al que hace referencia no ha sido fabricado");
+            return 0;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", ProductoPerecedero {" +
+                "fFabricacion=" + fFabricacion +
+                ", fCaducidad=" + fCaducidad +
+                ", días hasta caducidad=" + tiempoConsumicion() +
+                '}';
+    }
+}
