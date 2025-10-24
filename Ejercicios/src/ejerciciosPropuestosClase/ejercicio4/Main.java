438c42d5183adf040b75dd9d3c5598c9e55e3164
Alberto <albertodepablolopez@gmail.com>
2025-10-24 12:59:30 +0200

    Ejercicio4 EjerciciosPropuestos

---
+++ Ejercicios/src/ejerciciosPropuestosClase/ejercicio4/Main.java
@@ -0,0 +1,11 @@
+package ejerciciosPropuestosClase.ejercicio4;
+
+public class Main {
+    public static void main(String[] args) {
+        Campos c1 = new Campos();
+
+        c1.mostrar();
+        c1.incrementar();
+        c1.mostrar();
+    }
+}
---
+++ Ejercicios/src/ejerciciosPropuestosClase/ejercicio5/Main.java
@@ -0,0 +1,10 @@
+package ejerciciosPropuestosClase.ejercicio5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TablaMultiplicar tabla = new TablaMultiplicar(7);
+        System.out.println(tabla.generarTabla());
+    }
+}
+
