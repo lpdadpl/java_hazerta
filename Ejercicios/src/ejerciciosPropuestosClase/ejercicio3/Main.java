9e065655bfb5b644d3cf1f6c55f8592d41b72803
Alberto <albertodepablolopez@gmail.com>
2025-10-24 12:44:18 +0200

    Ejercicio3 EjercicioPropuestos

---
+++ Ejercicios/src/ejerciciosPropuestosClase/ejercicio3/Main.java
@@ -0,0 +1,14 @@
+package ejerciciosPropuestosClase.ejercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Areas Circulo = new Areas();
+
+        Areas Rectangulo= new Areas();
+
+        Circulo.calculoAreaCirculo(4);
+
+        Rectangulo.calculoAreaRectangulo(4, 6);
+    }
+}
