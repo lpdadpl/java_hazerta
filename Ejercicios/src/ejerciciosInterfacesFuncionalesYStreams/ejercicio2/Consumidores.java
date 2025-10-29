cb15fee70c69ca7cf5ee203c4994f03c147760a1
Alberto <albertodepablolopez@gmail.com>
2025-10-29 12:49:00 +0100

    chore(git): centralize ignores at root, remove nested .gitignore, and stop tracking IDE/build outputs

---
+++ Ejercicios/src/ejerciciosInterfacesFuncionalesYStreams/ejercicio2/Consumidores.java
@@ -0,0 +1,30 @@
+package ejerciciosInterfacesFuncionalesYStreams.ejercicio2;
+
+/// 1. Consumidor para imprimir en mayúsculas: Tomar una cadena de texto y
+/// mostrarla en mayúsculas.
+/// 2. Consumidor para imprimir números y sus cuadrados: Tomar un número
+/// entero y mostrarlo junto con su cuadrado.
+/// 3. Consumidor para imprimir detalles de un producto: Tomar un objeto Producto
+/// y mostrar sus detalles como nombre, precio, etc.
+
+public class Consumidores {
+
+    private void pasarMayus(){
+
+    }
+
+    private void numerosYCuadrados(){
+
+    }
+
+    private void imprimirDetalles(){
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
